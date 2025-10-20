99f1a946df8442a4780e2798f0582e6b01fc88d0
fabio <fabio@example.com>
2025-10-19 21:19:51 -0300

    feat(backend): created ormconfig.js

---
+++ ormconfig.js
@@ -0,0 +1,20 @@
+const { DataSource } = require('typeorm');
+
+module.exports = new DataSource({
+  type: 'postgres',
+  host: process.env.DB_HOST || 'localhost',
+  port: parseInt(process.env.DB_PORT || '5432'),
+  username: process.env.DB_USERNAME || 'fenix_user',
+  password: process.env.DB_PASSWORD || 'fenix_password',
+  database: process.env.DB_DATABASE || 'fenix_db',
+  entities: ['src/**/*.entity.ts'],
+  migrations: ['src/migrations/*.ts'],
+  synchronize: false,
+  logging: true,
+});
+
+
+
+
+
+
