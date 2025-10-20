8bba05430ce615f0d9f4c406caca5f1f9702578f
fabio <fabio@example.com>
2025-10-19 21:20:47 -0300

    feat(test): created test controllers for development

---
+++ src/test-prazos.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('test-prazos')
+export class TestPrazosController {
+  @Get()
+  test() {
+    return { message: 'Test prazos working!' };
+  }
+}
+
+
---
+++ src/test-simple.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('test-simple')
+export class TestSimpleController {
+  @Get()
+  test() {
+    return { message: 'Test simple working!' };
+  }
+}
+
+
