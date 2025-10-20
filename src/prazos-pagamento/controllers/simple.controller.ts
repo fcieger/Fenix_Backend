ca29c9f1ae080c6443fbca467155631456d67a8a
fabio <fabio@example.com>
2025-10-19 21:20:24 -0300

    feat(prazos-pagamento): created payment terms module with controllers and services

---
+++ src/prazos-pagamento/controllers/simple.controller.ts
@@ -0,0 +1,11 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('simple-prazos')
+export class SimpleController {
+  @Get()
+  test() {
+    return { message: 'Simple prazos working!' };
+  }
+}
+
+
