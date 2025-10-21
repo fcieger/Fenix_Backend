b2220cdfce34a902668078a6480404ee2a6aa19c
Fabio <fabio@example.com>
2025-10-21 15:25:08 -0300

    feat(prazos-pagamento/controllers): created simple.controller

---
+++ prazos-pagamento/controllers/simple.controller.ts
@@ -0,0 +1,15 @@
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('simple-prazos')
+export class SimpleController {
+  @Get()
+  test() {
+    return { message: 'Simple prazos working!' };
+  }
+}
+
+
+
+
+
+
