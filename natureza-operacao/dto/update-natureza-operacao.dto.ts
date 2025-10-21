965f7501274ca1714efe6ac509eb7451dc8eae15
Fabio <fabio@example.com>
2025-10-21 15:21:20 -0300

    feat(natureza-operacao/dto): created update-natureza-operacao.dto

---
+++ natureza-operacao/dto/update-natureza-operacao.dto.ts
@@ -0,0 +1,9 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateNaturezaOperacaoDto } from './create-natureza-operacao.dto';
+
+export class UpdateNaturezaOperacaoDto extends PartialType(CreateNaturezaOperacaoDto) {}
+
+
+
+
+
