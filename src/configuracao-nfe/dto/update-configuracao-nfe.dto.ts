d925568baa57c889198979ad149246f9cca38874
fabio <fabio@example.com>
2025-10-24 13:28:12 -0300

    feat(configuracao-nfe): created configuracao-nfe module

---
+++ src/configuracao-nfe/dto/update-configuracao-nfe.dto.ts
@@ -0,0 +1,18 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateConfiguracaoNfeDto } from './create-configuracao-nfe.dto';
+
+export class UpdateConfiguracaoNfeDto extends PartialType(CreateConfiguracaoNfeDto) {}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
