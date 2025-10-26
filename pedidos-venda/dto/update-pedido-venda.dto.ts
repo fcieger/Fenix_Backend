141257a94209a8dda79816105b8aa509cd0d7692
imLeonam <leonammrib@gmail.com>
2025-10-26 01:36:29 -0300

    refactor(pedidos-venda): removed update-pedido-venda.dto.ts from root directory

--- pedidos-venda/dto/update-pedido-venda.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreatePedidoVendaDto } from './create-pedido-venda.dto';
-
-export class UpdatePedidoVendaDto extends PartialType(CreatePedidoVendaDto) {}
