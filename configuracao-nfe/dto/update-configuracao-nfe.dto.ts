0d791311dbbba44bd871bdc6e87165406aec2941
imLeonam <leonammrib@gmail.com>
2025-10-26 01:35:35 -0300

    refactor(configuracao-nfe): removed update-configuracao-nfe.dto.ts from root directory

--- configuracao-nfe/dto/update-configuracao-nfe.dto.ts
+++
@@ -1,7 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateConfiguracaoNfeDto } from './create-configuracao-nfe.dto';
-
-export class UpdateConfiguracaoNfeDto extends PartialType(CreateConfiguracaoNfeDto) {}
-
-
-
