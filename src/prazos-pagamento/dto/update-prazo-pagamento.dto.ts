98a62dd1834977cf8b227ed0d617b84a9fcaadef
imLeonam <leonammrib@gmail.com>
2025-10-26 01:34:33 -0300

    refactor(prazos-pagamento): improved formatting in update-prazo-pagamento.dto.ts

--- src/prazos-pagamento/dto/update-prazo-pagamento.dto.ts
+++ src/prazos-pagamento/dto/update-prazo-pagamento.dto.ts
@@ -1,26 +1,6 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreatePrazoPagamentoDto } from './create-prazo-pagamento.dto';
 
-export class UpdatePrazoPagamentoDto extends PartialType(CreatePrazoPagamentoDto) {}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+export class UpdatePrazoPagamentoDto extends PartialType(
+  CreatePrazoPagamentoDto,
+) {}
