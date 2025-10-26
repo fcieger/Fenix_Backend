4eedba8def5c1205a521b84d40e854fd569047e8
imLeonam <leonammrib@gmail.com>
2025-10-26 01:36:45 -0300

    refactor(prazos-pagamento): removed test.controller.ts from root directory

--- prazos-pagamento/controllers/test.controller.ts
+++
@@ -1,9 +0,0 @@
-import { Controller, Get } from '@nestjs/common';
-
-@Controller('test-prazos')
-export class TestController {
-  @Get()
-  test() {
-    return { message: 'Prazos module is working!' };
-  }
-}
