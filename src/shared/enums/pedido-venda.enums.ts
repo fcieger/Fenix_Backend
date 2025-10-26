5b99b0b2ea938946be736ee1f64be38545e45573
imLeonam <leonammrib@gmail.com>
2025-10-26 01:34:42 -0300

    refactor(shared): improved formatting in pedido-venda.enums.ts

--- src/shared/enums/pedido-venda.enums.ts
+++ src/shared/enums/pedido-venda.enums.ts
@@ -6,24 +6,24 @@ export enum StatusPedido {
   ENTREGUE = 4,
   FATURADO = 5,
   CANCELADO = -1,
-  DEVOLVIDO = -2
+  DEVOLVIDO = -2,
 }
 
 export enum TipoFrete {
-  EMITENTE = 0,        // 0 - Por conta do emitente
-  DESTINATARIO = 1,    // 1 - Por conta do destinatário/remetente
-  TERCEIROS = 2,       // 2 - Por conta de terceiros
-  SEM_FRETE = 9        // 9 - Sem cobrança de frete
+  EMITENTE = 0, // 0 - Por conta do emitente
+  DESTINATARIO = 1, // 1 - Por conta do destinatário/remetente
+  TERCEIROS = 2, // 2 - Por conta de terceiros
+  SEM_FRETE = 9, // 9 - Sem cobrança de frete
 }
 
 export enum IndicadorPresenca {
-  NAO_APLICA = 0,      // 0 - Não se aplica
-  PRESENCIAL = 1,      // 1 - Presencial
-  INTERNET = 2,        // 2 - Internet
+  NAO_APLICA = 0, // 0 - Não se aplica
+  PRESENCIAL = 1, // 1 - Presencial
+  INTERNET = 2, // 2 - Internet
   TELEATENDIMENTO = 3, // 3 - Teleatendimento
-  NFCe_DOMICILIO = 4,  // 4 - NFCe entrega em domicílio
+  NFCe_DOMICILIO = 4, // 4 - NFCe entrega em domicílio
   PRESENCIAL_FORA = 5, // 5 - Presencial fora do estabelecimento
-  OUTROS = 9           // 9 - Outros
+  OUTROS = 9, // 9 - Outros
 }
 
 export enum FormaPagamento {
@@ -38,7 +38,7 @@ export enum FormaPagamento {
   VALE_COMBUSTIVEL = 13,
   BOLETO_BANCARIO = 15,
   SEM_PAGAMENTO = 90,
-  OUTROS = 99
+  OUTROS = 99,
 }
 
 export enum TipoEstoque {
@@ -46,5 +46,5 @@ export enum TipoEstoque {
   RESERVA = 2,
   EXIBICAO = 3,
   CONSIGNACAO = 4,
-  OUTROS = 9
+  OUTROS = 9,
 }
