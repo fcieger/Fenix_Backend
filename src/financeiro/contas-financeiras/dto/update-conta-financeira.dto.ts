1c78d3897b051aa9d13e5b1542c3501d27088d1e
imLeonam <leonammrib@gmail.com>
2025-10-26 01:37:17 -0300

    refactor(financeiro): removed update-conta-financeira.dto.ts from root directory

--- src/financeiro/contas-financeiras/dto/update-conta-financeira.dto.ts
+++
@@ -1,5 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateContaFinanceiraDto } from './create-conta-financeira.dto';
-
-export class UpdateContaFinanceiraDto extends PartialType(CreateContaFinanceiraDto) {}
-
