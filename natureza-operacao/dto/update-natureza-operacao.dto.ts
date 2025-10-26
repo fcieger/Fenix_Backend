ce378a63c41d12a47caa55953ca70c7025230e03
imLeonam <leonammrib@gmail.com>
2025-10-26 01:35:56 -0300

    refactor(natureza-operacao): removed update-natureza-operacao.dto.ts from root directory

--- natureza-operacao/dto/update-natureza-operacao.dto.ts
+++
@@ -1,9 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateNaturezaOperacaoDto } from './create-natureza-operacao.dto';
-
-export class UpdateNaturezaOperacaoDto extends PartialType(CreateNaturezaOperacaoDto) {}
-
-
-
-
-
