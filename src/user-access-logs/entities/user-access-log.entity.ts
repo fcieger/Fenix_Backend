992ae379f3adf7f6716194dc05910afab934f99c
imLeonam <leonammrib@gmail.com>
2025-10-26 01:34:46 -0300

    refactor(user-access-logs): improved formatting in user-access-log.entity.ts

--- src/user-access-logs/entities/user-access-log.entity.ts
+++ src/user-access-logs/entities/user-access-log.entity.ts
@@ -1,4 +1,12 @@
-import { Entity, PrimaryGeneratedColumn, Column, ManyToOne, JoinColumn, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  ManyToOne,
+  JoinColumn,
+  CreateDateColumn,
+  UpdateDateColumn,
+} from 'typeorm';
 import { User } from '../../users/entities/user.entity';
 
 @Entity('user_access_logs')
