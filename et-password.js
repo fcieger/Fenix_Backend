f3d7e5f51713225f315f33e035fc1a5825dba37d
fcieger <fcieger1982@gmail.com>
2025-10-31 22:57:27 -0300

    feat(scripts): created et-password

---
+++ et-password.js
@@ -0,0 +1,9 @@
+[1mdiff --git a/reset-password.js b/reset-password.js[m
+[1mindex 88fec50..72bc4f3 100644[m
+[1m--- a/reset-password.js[m
+[1m+++ b/reset-password.js[m
+[36m@@ -21,3 +21,4 @@[m [masync function resetPassword() {[m
+ }[m
+ [m
+ resetPassword();[m
+[41m+[m
